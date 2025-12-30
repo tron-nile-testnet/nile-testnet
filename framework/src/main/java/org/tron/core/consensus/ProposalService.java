19b9c9507841f4e1bb53d80a45a4fece11aa7f17
albedo xu <511861287@qq.com>
2025-12-30 15:13:04 +0800

    feat(*): disable market transaction

--- framework/src/main/java/org/tron/core/consensus/ProposalService.java
+++ framework/src/main/java/org/tron/core/consensus/ProposalService.java
@@ -217,11 +217,11 @@ public class ProposalService extends ProposalUtil {
           break;
         }
         case ALLOW_MARKET_TRANSACTION: {
-          if (manager.getDynamicPropertiesStore().getAllowMarketTransaction() == 0) {
-            manager.getDynamicPropertiesStore().saveAllowMarketTransaction(entry.getValue());
+         // if (manager.getDynamicPropertiesStore().getAllowMarketTransaction() == 0) {
+            manager.getDynamicPropertiesStore().saveAllowMarketTransaction(0);
             manager.getDynamicPropertiesStore().addSystemContractAndSetPermission(52);
             manager.getDynamicPropertiesStore().addSystemContractAndSetPermission(53);
-          }
+         // }
           break;
         }
         case MARKET_SELL_FEE: {
