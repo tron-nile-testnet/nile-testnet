44a4bc826383f089e03351124edebf4190d41eb4
YAaron <4241080+kuny0707@users.noreply.github.com>
2026-02-04 11:25:01 +0800

    Merge release_v4.8.1 to master (#6541)

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.8.0-1-g45e3bf88ca";
   public static final String VERSION_CODE = "18634";
-  private static final String VERSION = "4.8.0.1";
+  private static final String VERSION = "4.8.1";
 
   public static String getVersion() {
     return VERSION;
