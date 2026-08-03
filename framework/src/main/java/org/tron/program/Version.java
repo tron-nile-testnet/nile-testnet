8edb6acfc74b6d95d61165ac778406a5a0d106e7
tron-nile-testnet <niletestnet@gmail.com>
2026-08-03 16:41:39 +0800

    update a new version. version name:GreatVoyage-Nile-v4.8.2-PQ1-build2-14-g5209c4298,version code:19268

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-Nile-v4.8.2-PQ1-build1-17-gbee79f7b4";
-  public static final String VERSION_CODE = "19252";
+  public static final String VERSION_NAME = "GreatVoyage-Nile-v4.8.2-PQ1-build2-14-g5209c4298";
+  public static final String VERSION_CODE = "19268";
   private static final String VERSION = "4.8.2.PQ1_build2";
 
   public static String getVersion() {
