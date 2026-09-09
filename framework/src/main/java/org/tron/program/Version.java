55e4abebd129a69fdde3c5fc48b637ebb9ef27ab
tron-nile-testnet <niletestnet@gmail.com>
2026-09-09 14:07:59 +0800

    update version

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-Nile-v4.8.2.1-PQ1-build1-6-gfb17016c5";
   public static final String VERSION_CODE = "19278";
-  private static final String VERSION = "4.8.2.1.PQ1_build1";
+  private static final String VERSION = "4.8.2.2.PQ1_build1";
 
   public static String getVersion() {
     return VERSION;
